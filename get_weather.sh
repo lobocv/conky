cb61327e69b1ad78c895fa4de4cbb10f3db02a2b
Calvin <calvinlobo@gmail.com>
2015-04-04 12:24:51 -0400

    fixes to get_weather.sh temp and weather

--- get_weather.sh
+++ get_weather.sh
@@ -1,29 +1,21 @@
 #!/bin/sh
-#AccuWeather (r) RSS weather tool for conky
-#
-#USAGE: weather.sh <locationcode>
-#
-#(c) Michael Seiler 2007
+# Weather tool for conky
+#USAGE: get_weather.sh <field>
+#(c) Calvin Lobo 2015
 
 METRIC=1 #Should be 0 or 1; 0 for F, 1 for C
 ICAO=CYYZ
 URL=http://weather.noaa.gov/pub/data/observations/metar/decoded/$ICAO.TXT
 TMP_FILE=/tmp/local_weather.txt
-#if [ -z $1 ]; then
-#    echo
-#    echo "USAGE: weather.sh <locationcode>"
-#    echo
-#    exit 0;
-#fi
-
-
 curl -s $URL  > $TMP_FILE
 
 
 if [ "$1" = "temp" ]; then
-	echo `grep "^Temperature" $TMP_FILE | grep "[0-9]\{2\} C" -o`
+	echo `grep "^Temperature" $TMP_FILE | grep "[0-9]* C" -o`
 elif [ "$1" = "weather" ]; then
-	echo `grep "^Weather" $TMP_FILE | cut -c 10-`
+	echo `grep "^Weather" $TMP_FILE | cut -c 10-` `grep "^Sky conditions:" $TMP_FILE | cut -c 17-`
+elif [ "$1" = "all" ]; then
+	echo `cat $TMP_FILE`
 fi
 
 
